3c3e6a89ed5f809fc78bb669d3f69626dbf695b9
Juanes-TM <alu0101592916@ull.edu.es>
2026-05-04 17:40:21 +0100

    fix: solución de rutas

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,13 +1,11 @@
 import { connect } from "mongoose";
 
 export async function connectDB() {
-  const db = process.env.NODE_ENV === "test"
-  ? process.env.MONGODB_TEST_URI
-  : process.env.MONGODB_URL;
-  if (!db)
+  const URL = process.env.MONGODB_URL;
+  if (!URL)
     throw new Error(`MONGODB_URL is not defined in environment variables`);
   try {
-    await connect(db, { connectTimeoutMS: 10000 });
+    await connect(URL, { connectTimeoutMS: 10000 });
     console.log(`Connection to MongoDB server established`);
   } catch (error) {
     console.log(`Unable to connect to MongoDB server:`, error);
