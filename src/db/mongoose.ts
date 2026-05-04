333769c97d1ab771ef9c231a5213c31b0c726bbe
Juanes-TM <alu0101592916@ull.edu.es>
2026-05-04 17:34:51 +0100

    feat: permite una bd para los test

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,11 +1,13 @@
 import { connect } from "mongoose";
 
 export async function connectDB() {
-  const URL = process.env.MONGODB_URL;
-  if (!URL)
+  const db = process.env.NODE_ENV === "test"
+  ? process.env.MONGODB_TEST_URI
+  : process.env.MONGODB_URL;
+  if (!db)
     throw new Error(`MONGODB_URL is not defined in environment variables`);
   try {
-    await connect(URL, { connectTimeoutMS: 10000 });
+    await connect(db, { connectTimeoutMS: 10000 });
     console.log(`Connection to MongoDB server established`);
   } catch (error) {
     console.log(`Unable to connect to MongoDB server:`, error);
