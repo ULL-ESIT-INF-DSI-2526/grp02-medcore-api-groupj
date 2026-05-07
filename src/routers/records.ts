feff090ee780d6c4f35b46c204150dda8b8f53b6
Juanes-TM <alu0101592916@ull.edu.es>
2026-05-07 03:13:33 +0100

    feat: avance del manejador post para record

---
+++ src/routers/records.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import { validateMainData } from "../utils/records/validate/validateMainData.js";
+import { getMedicationID } from "../utils/records/id/getMedicationID.js";
+import { getPatientID } from "../utils/records/id/getPatientID.js";
+import { getStaffID } from "../utils/records/id/getStaffID.js";
+
+
+export const recordRouter = express.Router();
+
+recordRouter.post("/record", async (req, res) => {
+  try {
+    const {idDocument, medicalLicense, medicationList} = req.body;
+    validateMainData(idDocument, medicalLicense, medicationList);
+    const patient = await getPatientID(idDocument);
+    const responsibleStaff = await getStaffID(medicalLicense);
+    const idMedicationList = await getMedicationID(medicationList);
+  }
+  catch {
+
+  }
+})
