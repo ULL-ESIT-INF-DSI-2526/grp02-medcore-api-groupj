79919858ff7549e80afa1732ad3358b2e28b7bef
Juanes-TM <alu0101592916@ull.edu.es>
2026-05-08 18:27:49 +0100

    feat: se añadio GET /records y una funcion para validad sus datos

--- src/routers/records.ts
+++ src/routers/records.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { Record } from "../models/records.js";
 import { validateMainData } from "../utils/records/validate/validateMainData.js";
 import { validateDates } from "../utils/records/validate/validateDate.js";
+import { validateGetData } from "../utils/records/validate/validateGetData.js";
 import { processMedications } from "../utils/records/id/processMedications.js";
 import { RollbackMedication } from "../types/consultas/rollbackMedication.js";
 import { AppError } from "../utils/records/error/appError.js";
@@ -65,6 +66,39 @@ recordRouter.post("/records", async (req, res) => {
       else if (error.name === "ValidationError") {
         return res.status(400).send({error: error.message});
       }
+    }
+    return res.status(500).send({error: "Error interno del servidor"});
+  }
+})
+
+recordRouter.get("/records", async (req, res) => {
+  try {
+    const {idDocument, startDate, endDate, recordType} = req.query;
+    validateGetData(idDocument, startDate, endDate, recordType);
+    if (idDocument) {
+      const patientId = await getPatientID(idDocument as any);
+      const records = await Record.find({ patient: patientId}).sort({ admissionDateTime: 1});
+      if (records.length === 0) {
+        return res.status(404).send({error: "No se encontraron registros"});
+      }
+      return res.send(records);
+    }
+    const filter: any = {
+      admissionDateTime: {
+        $gte: new Date(startDate as string),
+        $lte: new Date(endDate as string)
+      }
+    };
+    if (recordType) filter.recordType = recordType;
+    const records = await Record.find(filter).sort({admissionDateTime: 1});
+    if (records.length === 0) {
+      return res.status(404).send({error: "No se encontraron registros"});
+    }
+    res.send(records);
+  }
+  catch (error: unknown) {
+    if (error instanceof ValidationErrors) {
+      return res.status(error.statusCode).send({error: error.message, details: error.errors});
     }
     return res.status(500).send({error: "Error interno del servidor"});
   }
