d07deb2d0a41910b985cc805be63dec8103444be
Juanes-TM <alu0101592916@ull.edu.es>
2026-05-09 06:22:18 +0100

    feat: se añadio PATCH /records/:id y funciones para su implementacion

--- src/routers/records.ts
+++ src/routers/records.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import { Record } from "../models/records.js";
+import { Medication } from "../models/medications.js";
 import { validateMainData } from "../utils/records/validate/validateMainData.js";
+import { validateArray } from "../utils/records/validate/validateMainData.js";
+import { validateString } from "../utils/records/validate/validateMainData.js";
 import { validateDates } from "../utils/records/validate/validateDate.js";
+import { validatePatchDates } from "../utils/records/validate/validatePatchDates.js";
+import { validatePatchBody } from "../utils/records/validate/validatePatchBody.js";
 import { validateGetData } from "../utils/records/validate/validateGetData.js";
 import { processMedications } from "../utils/records/id/processMedications.js";
 import { RollbackMedication } from "../types/consultas/rollbackMedication.js";
@@ -10,7 +15,8 @@ import { AppError } from "../utils/records/error/appError.js";
 import { ValidationErrors } from "../utils/records/error/validationErrors.js";
 import { getPatientID } from "../utils/records/id/getPatientID.js";
 import { getStaffID } from "../utils/records/id/getStaffID.js";
-import { rollbackMedicationStock } from "../utils/records/rollback/rollbackMedicationStock.js";
+import { updateMedicationStock } from "../utils/records/rollback/updateMedicationStock.js";
+import { getRollback } from "../utils/records/rollback/getRollback.js";
 
 export const recordRouter = express.Router();
 
@@ -53,7 +59,7 @@ recordRouter.post("/records", async (req, res) => {
   } catch (error: unknown) {
     if (rollback.length > 0) {
       try {
-        await rollbackMedicationStock(rollback);
+        await updateMedicationStock(rollback, 1);
       } catch (rollbackError) {
         console.error(rollbackError);
       }
@@ -111,7 +117,7 @@ recordRouter.get("/records", async (req, res) => {
   }
 });
 
-recordRouter.get("records/:id", async (req, res) => {
+recordRouter.get("/records/:id", async (req, res) => {
   try {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -119,10 +125,100 @@ recordRouter.get("records/:id", async (req, res) => {
     }
     const record = await Record.findById(id);
     if (!record) {
-      return res.status(404).send({ error: "No se encontro la consulta" });
+      return res.status(404).send({ error: "No se encontro el registro" });
     }
     res.send(record);
   } catch {
     res.status(500).send({ error: "Error interno del servidor" });
   }
 });
+
+recordRouter.patch("/records/:id", async (req, res) => {
+  const rollback1: RollbackMedication[] = [];
+  const rollback2: RollbackMedication[] = [];
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({ error: "ID invalido" });
+    }
+    const record = await Record.findById(id);
+    if (!record) {
+      return res.status(404).send({ error: "No se encontro el registro" });
+    }
+    validatePatchBody(req.body);
+    const {
+      idDocument,
+      medicalLicense,
+      medicationList,
+      recordType,
+      admissionDateTime,
+      dischargeDateTime,
+      reason,
+      diagnosis,
+      recordStatus,
+    } = req.body;
+    const errors: string[] = [];
+    if (idDocument) validateString(idDocument, errors, true);
+    if (medicalLicense) validateString(medicalLicense, errors, false);
+    if (errors.length > 0) throw new ValidationErrors(errors);
+    validatePatchDates(record, admissionDateTime, dischargeDateTime, recordStatus);
+    let prescribedMedications;
+    let amount;
+    if (medicationList) {
+      validateArray(medicationList, errors);
+      if (errors.length > 0) throw new ValidationErrors(errors);
+      getRollback(record.prescribedMedications, rollback1);
+      await updateMedicationStock(rollback1, 1);
+      ({prescribedMedications, amount} = await processMedications(medicationList, rollback2));
+    }
+    if (idDocument) record.patient = await getPatientID(idDocument);
+    if (medicalLicense) record.responsibleStaff = await getStaffID(medicalLicense);
+    if (recordType) record.recordType = recordType;
+    if (admissionDateTime) record.admissionDateTime = new Date(admissionDateTime);
+    if (recordStatus) record.recordStatus = recordStatus;
+    if (reason) record.reason = reason;
+    if (diagnosis) record.diagnosis = diagnosis;
+    if (prescribedMedications) record.prescribedMedications = prescribedMedications;
+    if (amount !== undefined) record.amount = amount;
+    if (record.recordStatus === "abierto") record.dischargeDateTime = undefined;
+    else {
+      if (dischargeDateTime) {
+        record.dischargeDateTime = new Date(dischargeDateTime);
+      }
+      else if (!record.dischargeDateTime) {
+        record.dischargeDateTime = new Date();
+      }
+    }
+    await record.save();
+    res.send(record);
+  }
+  catch (error: unknown) {
+    if (rollback2.length > 0) {
+      try {
+        await updateMedicationStock( rollback2, 1 );
+      }
+      catch (rollbackError) {
+        console.error(rollbackError);
+      }
+    }
+    if (rollback1.length > 0) {
+      try {
+        await updateMedicationStock( rollback1, -1);
+      }
+      catch (rollbackError) {
+        console.error(rollbackError);
+      }
+    }
+    if (error instanceof ValidationErrors) {
+      return res.status(error.statusCode).send({ error: error.message, details: error.errors });
+    }
+    else if (error instanceof AppError) {
+      return res.status(error.statusCode).send({ error: error.message });
+    }
+    else if (error instanceof Error && error.name === "ValidationError") {
+      return res.status(400).send({error: error.message});
+    }
+    return res.status(500).send({error: "Error interno del servidor"});
+  }
+});
+
--- src/utils/records/rollback/updateMedicationStock.ts
+++ src/utils/records/rollback/updateMedicationStock.ts
@@ -1,15 +1,15 @@
 import { RollbackMedication } from "../../../types/consultas/rollbackMedication.js";
 import { Medication } from "../../../models/medications.js";
-import { NotFoundError } from "../error/notFoundError.js";
 
-export async function rollbackMedicationStock( rollback: RollbackMedication[]): Promise<void> {
-  for (const item of rollback) {
+export async function updateMedicationStock(medications: RollbackMedication[],
+                                            operation: 1 | -1): Promise<void> {
+  for (const item of medications) {
     const medication = await Medication.findById(item.medication);
     if (!medication) {
       console.error(`No se encontró el medicamento con id ${item.medication}`);
       continue;
     }
-    medication.stockDisponible += item.units;
+    medication.stockDisponible += item.units * operation;
     await medication.save();
   }
 }
